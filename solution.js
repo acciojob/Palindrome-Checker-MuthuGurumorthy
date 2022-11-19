31bc9e41cd2ff90be9469f7ab1e7a0e8ab2cbf33
MuthuGurumorthy <88612046+MuthuGurumorthy@users.noreply.github.com>
2022-11-19 11:51:47 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
+  const len = str.length;
+  for (let i = 0; i < len / 2; i++) {
 
+        // check if first and last string are same
+        if (str[i] !== str[len - 1 - i]) {
+            return 'fasle';
+        }
+    }
+    return 'false';
 }
 module.exports = palindrome
